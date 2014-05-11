c1f329ee14083ab7794d38af6e934488c8d7c65e
Lars Hupel <lars.hupel@mytum.de>
2014-05-11 22:29:43 +0200

    Solution to Exercise 4

--- src/main/scala/ex4.scala
+++ src/main/scala/ex4.scala
@@ -3,6 +3,8 @@ package flatmap
 import scalaz._
 import scalaz.concurrent._
 import scalaz.effect._
+import scalaz.syntax.either._
+import scalaz.syntax.std.list._
 
 object Exercise4 {
 
@@ -11,18 +13,37 @@ object Exercise4 {
   // Find all related items to a given item which a given customer bought.
 
   def boughtRelated(customerID: Long, itemName: String): IO[List[Long]] =
-    ???
+    for {
+      itemID <- WebService.lookupItem(itemName)
+      related <- WebService.relatedItems(itemID)
+      filtered <- related.filterM(WebService.customerBoughtItem(customerID, _))
+    } yield filtered
 
   // Task 2: `WebService` has been refactored to be asynchronous and use `Task`.
   // However, now the API exposes errors.
   // Rewrite `boughtRelated` to deal with `Task` and errors.
   
   def boughtRelated2(customerID: Long, itemName: String): Task[WebService.ErrorCode \/ List[Long]] =
-    ???
+    WebService.lookupItem2(itemName) flatMap {
+      case \/-(itemID) =>
+        WebService.relatedItems2(itemID) flatMap {
+          case \/-(related) =>
+            ??? // oh god please stahp
+          case -\/(error) =>
+            Task.now(error.left)
+        }
+      case -\/(error) =>
+        Task.now(error.left)
+    }
 
   // Task 3: Strip some boilerplate with `EitherT`!
 
   def boughtRelated3(customerID: Long, itemName: String): EitherT[Task, WebService.ErrorCode, List[Long]] =
-    ???
+    for {
+      itemID <- EitherT(WebService.lookupItem2(itemName))
+      related <- EitherT(WebService.relatedItems2(itemID))
+      // or even a type lambda
+      filtered <- Unapply[Applicative, EitherT[Task, WebService.ErrorCode, Any]].TC.filterM(related)(item => EitherT(WebService.customerBoughtItem2(customerID, item)))
+    } yield filtered
 
 }
